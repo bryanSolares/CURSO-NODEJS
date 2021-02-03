7c57ec35017aac50df12e7294c1d7c832090073e
ubuntu-book <solares.josue@outlook.com>
2021-02-02 22:31:22 -0600

    Reconfiguracion Limpieza

--- node/07-restserver/src/middlewares/autenticacion.js
+++ node/07-restserver/src/middlewares/autenticacion.js
@@ -3,11 +3,7 @@ const { SEED } = require("../config/config");
 
 const verificaToken = (req, res, next) => {
   const token = req.get("x-token") || "";
-<<<<<<< HEAD
   jwt.verify(token, SEED, (error, decoded) => {
-=======
-  jwt.verify(token, process.env.SEED || SEED, (error, decoded) => {
->>>>>>> b7b6cdbaafc7ec7c27dce51bbf6af5e77d2c8b53
     if (error) {
       res.status(401).json({ ok: false, error, msg: "Invalid Token" });
     }
