6e8d51038016936b3a9d010c95f4656ff356ef27
ubuntu-book <solares.josue@outlook.com>
2021-02-07 18:35:09 -0600

    Fin seccion 10

--- node/07-restserver/src/middlewares/autenticacion.js
+++ node/07-restserver/src/middlewares/autenticacion.js
@@ -2,16 +2,36 @@ const jwt = require("jsonwebtoken");
 const { SEED } = require("../config/config");
 
 const verificaToken = (req, res, next) => {
+  let response = { error: null, ok: false, msg: "" };
   const token = req.get("x-token") || "";
   jwt.verify(token, SEED, (error, decoded) => {
     if (error) {
-      res.status(401).json({ ok: false, error, msg: "Invalid Token" });
+      response.error = error;
+      response.msg = "Invalid Token";
+      res.status(401).json(response);
     }
 
     req.usuario = decoded.usuario;
-    console.log(decoded);
     next();
   });
 };
 
-module.exports = { verificaToken };
+const verificaAdminRole = (req, res, next) => {
+  let response = { error: null, ok: false, msg: "" };
+  try {
+    const { role } = req.usuario;
+
+    if (role !== "ADMIN_ROLE") {
+      response.msg = "Solo un usuario administrador puede realizar esta accion";
+      return res.json(response);
+    }
+
+    next();
+  } catch (error) {
+    response.error = error;
+    response.msg = "Error en verificar los permisos del usuario";
+    res.json(response);
+  }
+};
+
+module.exports = { verificaToken, verificaAdminRole };
--- node/07-restserver/src/routes/usuario.js
+++ node/07-restserver/src/routes/usuario.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const controller = require("../controllers/usuario.controller");
-const { verificaToken } = require("../middlewares/autenticacion");
+const { verificaToken, verificaAdminRole } = require("../middlewares/autenticacion");
 
 router.get("/", verificaToken, controller.rutaInicial);
-router.post("/nuevo-usuario", verificaToken, controller.crearUsuario);
+router.post("/nuevo-usuario", [verificaToken, verificaAdminRole], controller.crearUsuario);
 router.put("/editar-usuario/:id", verificaToken, controller.modificarUsuario);
 router.get("/todos", verificaToken, controller.mostrarTodos);
-router.delete("/eliminar/:id", verificaToken, controller.eliminarUno);
+router.delete("/eliminar/:id", [verificaToken, verificaAdminRole], controller.eliminarUno);
 
 module.exports = router;
